29acd482a43b84266b9fbd14822a544199537138
Jeffdude <jeffmilling@gmail.com>
2021-02-22 19:43:00 +0000

    amplify init

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 export default {
-  backend_url: "http://192.168.1.202:3600/api/v1/",
+  //backend_url: "http://192.168.1.202:3600/api/v1/",
   //backend_url: "http://192.168.0.223:3600/api/v1/",
+  backend_url: "http://ambassadorsitebackend.eba-eyky589e.us-east-2.elasticbeanstalk.com/api/v1/",
   title_font: "Lato_400Regular",
   body_font: "Roboto_300Light",
   bold_body_font: "Roboto_500Medium",
 }
+
