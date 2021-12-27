165768e57682dcb39c0110d8c141476a4dabaa74
Jeffdude <jeffmilling@gmail.com>
2021-12-27 23:11:15 +0000

    Revert "update packages for node 14"
    This reverts commit 04c9b6914b7829ad981b25ea8212c50771a4202d.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   //backend_url: "http://192.168.1.202:3600/api/v1/",
-  //backend_url: "http://192.168.1.8:3600/api/v1/",
+  //backend_url: "http://192.168.0.223:3600/api/v1/",
   backend_url: "https://backend.ambassadorsite.jmkride-internal.link/api/v1/",
   title_font: "Lato_400Regular",
   body_font: "Roboto_300Light",
